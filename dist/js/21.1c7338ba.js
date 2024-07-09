d20cc73564b77aff5b33a3210440ab50eab90d6b
yaelkan1997 <yael.kanevsky@gmail.com>
2024-07-09 21:20:33 +0300

    build the project

---
+++ dist/js/21.1c7338ba.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkassignment_2_1"]=self["webpackChunkassignment_2_1"]||[]).push([[21],{8021:(s,t,e)=>{e.r(t),e.d(t,{default:()=>u});var n=function(){var s=this;s._self._c;return s._m(0)},l=[function(){var s=this,t=s._self._c;return t("div",{staticClass:"container"},[t("h1",{staticClass:"title"},[s._v("New Recipe")])])}],a=e(1001),i={},c=(0,a.Z)(i,n,l,!1,null,null,null);const u=c.exports}}]);
+//# sourceMappingURL=21.1c7338ba.js.map
